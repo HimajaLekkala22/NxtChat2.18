9e1b754ca5229e1642da08f3ef8e299d50a322d1
lekkala.sai <lekkala.sai@myn.global>
2024-07-05 17:11:53 +0530

    changed system avatar & login background

--- app/components/system_avatar/index.tsx
+++ app/components/system_avatar/index.tsx
@@ -13,7 +13,7 @@ type Props = {
 const SystemAvatar = ({theme}: Props) => {
     return (
         <CompassIcon
-            name='mattermost'
+            name='monitor'
             color={theme.centerChannelColor}
             size={ViewConstants.PROFILE_PICTURE_SIZE}
         />
